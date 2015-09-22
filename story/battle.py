95fd145c9a46e824c24440bd4b05001521eac7fc
Patrick Mays <Blueroofgames@gmail.com>
2015-09-21 20:44:49 -0400

    save

--- story/battle.py
+++ story/battle.py
@@ -5,5 +5,3 @@ You rush into battle with the second wave, arrows flying past your head and hitt
  You catch his sword with your sheild and cleave him over th head he falls to the ground and withers patheticly.
  with the next volley of arrows the orcs reteat. You won!
 """
-
-on_leave='exit'
--- story/road.py
+++ story/road.py
@@ -1,8 +1,10 @@
 """Road
 
-You are in a covered wagon with complete strangers. The wagon bumps lazily across the grvel road shifting the small stones.
-you scan the ravine you are in. All you can see is a few mountain goats and waterfalls trickeling down the clifsides, then
-you see large gates. do you want to venture closer?
+You are in a covered wagon with complete strangers. The wagon bumps
+lazily across the grvel road shifting the small stones.  you scan the
+ravine you are in. All you can see is a few mountain goats and waterfalls
+trickeling down the clifsides, then you see large gates. do you want to
+venture closer?
 """
 
 on_y="moria"
