fdc8b4f7b8f5501e21bf275714193a44d644a523
Patrick Mays <Blueroofgames@gmail.com>
2015-10-02 12:42:40 -0400

    save

--- story/moria.py
+++ story/moria.py
@@ -1,7 +1,7 @@
 """Moria
 
 You walk up and knock on the doors. A small slit creaks open in the doors and a dwarf peaks out.
- State your buisness!, the dwarf said. i heard rumbling in the distance and a man sprinted past ORCS!! He yelled.
+ State your buisness!, the dwarf said. I heard rumbling in the distance and a man sprinted past ORCS!! He yelled.
  Orcs at the gates A say the door swings open, I am handed a sword and shield I am told to keep them, if I fight.
  will tou fight or run like a coward?
 """
